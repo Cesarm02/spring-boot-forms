bf4ad5a207a0f4c69d8c6052e1f08e7d62cf3e71
Cesar Estiven Medrano <cesar-2007-mesa@hotmail.com>
2023-09-22 12:21:50 -0500

    validación form

--- src/main/java/com/example/form/controllers/FormController.java
+++ src/main/java/com/example/form/controllers/FormController.java
@@ -7,33 +7,43 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 
     @GetMapping("/form")
     public String form(Model model){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Cesar");
+        usuario.setApellido("Mesa");
+        usuario.setId("123215 - K");
         model.addAttribute("titulo", "Form usuarios");
-        model.addAttribute("usuario", new Usuario());
+        model.addAttribute("usuario", usuario);
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(@Valid @ModelAttribute("usuario") Usuario usuario, BindingResult result, Model model){
+    public String procesar(@Valid @ModelAttribute("usuario") Usuario usuario, BindingResult result, Model model, SessionStatus sessionStatus){
         model.addAttribute("titulo", "Resultado form");
         if(result.hasErrors()){
+           /*
             Map<String, String> errores = new HashMap<>();
             result.getFieldErrors().forEach(err ->{
                 errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
             });
             model.addAttribute("error", errores);
+            */
             return "form";
         }
         model.addAttribute("usuario", usuario);
+        sessionStatus.setComplete();
         return "resultado";
     }
 
--- src/main/java/com/example/form/models/Usuario.java
+++ src/main/java/com/example/form/models/Usuario.java
@@ -1,14 +1,27 @@
 package com.example.form.models;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class Usuario {
 
+    @NotEmpty(message = "El nombre no puede ser vacio")
+    private String nombre;
     @NotEmpty
+
+    private String apellido;
+    // "Doble back slash d significa de 0-9
+    @Pattern(regexp = "[0-9]{2}[.][\\d]{3}[.][\\d]{3}[-][A-Z]{1}")
+    private String id;
+    @NotEmpty
+    @Size(min = 3, max = 8)
     private String username;
     @NotEmpty
     private String password;
     @NotEmpty
+    @Email(message = "Formato invalido")
     private String email;
 
     public String getUsername() {
@@ -33,5 +46,29 @@ public class Usuario {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
